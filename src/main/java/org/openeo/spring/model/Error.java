a01cd8fe9e0ed1dad4eeeb3b806a80045757c68c
Alexander Jacob <alexander.jacob@eurac.edu>
2020-09-10 17:47:02 +0200

    fixed object mapping for persistence with hibernate

--- src/main/java/org/openeo/spring/model/Error.java
+++ src/main/java/org/openeo/spring/model/Error.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -13,158 +16,168 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
- * An error object declares additional information about a client-side or server-side error. See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json)
+ * An error object declares additional information about a client-side or
+ * server-side error. See also: * [Error
+ * Handling](#section/API-Principles/Error-Handling) in the API in general. *
+ * [Common Error Codes](errors.json)
  */
+@Entity
 @ApiModel(description = "An error object declares additional information about a client-side or server-side error. See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json)")
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-07-02T08:45:00.334+02:00[Europe/Rome]")
-public class Error   {
-  @JsonProperty("id")
-  private String id;
+public class Error {
+	
+	@Id
+	@JsonProperty("id")
+	private String id;
 
-  @JsonProperty("code")
-  private String code;
+	@JsonProperty("code")
+	private String code;
 
-  @JsonProperty("message")
-  private String message;
+	@JsonProperty("message")
+	private String message;
 
-  @JsonProperty("links")
-  @Valid
-  private List<Link> links = null;
+	@JsonProperty("links")
+	@Valid
+	@Embedded
+	private List<Link> links = null;
 
-  public Error id(String id) {
-    this.id = id;
-    return this;
-  }
+	public Error id(String id) {
+		this.id = id;
+		return this;
+	}
 
-  /**
-   * A back-end may add a unique identifier to the error response to be able to log and track errors with further non-disclosable details. A client could communicate this id to a back-end provider to get further information.
-   * @return id
-  */
-  @ApiModelProperty(example = "550e8400-e29b-11d4-a716-446655440000", value = "A back-end may add a unique identifier to the error response to be able to log and track errors with further non-disclosable details. A client could communicate this id to a back-end provider to get further information.")
+	/**
+	 * A back-end may add a unique identifier to the error response to be able to
+	 * log and track errors with further non-disclosable details. A client could
+	 * communicate this id to a back-end provider to get further information.
+	 * 
+	 * @return id
+	 */
+	@ApiModelProperty(example = "550e8400-e29b-11d4-a716-446655440000", value = "A back-end may add a unique identifier to the error response to be able to log and track errors with further non-disclosable details. A client could communicate this id to a back-end provider to get further information.")
 
+	public String getId() {
+		return id;
+	}
 
-  public String getId() {
-    return id;
-  }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-  public void setId(String id) {
-    this.id = id;
-  }
+	public Error code(String code) {
+		this.code = code;
+		return this;
+	}
 
-  public Error code(String code) {
-    this.code = code;
-    return this;
-  }
+	/**
+	 * The code is either one of the standardized error codes or a custom code, for
+	 * example specified by a user in the `debug` process.
+	 * 
+	 * @return code
+	 */
+	@ApiModelProperty(example = "SampleError", required = true, value = "The code is either one of the standardized error codes or a custom code, for example specified by a user in the `debug` process.")
+	@NotNull
 
-  /**
-   * The code is either one of the standardized error codes or a custom code, for example specified by a user in the `debug` process.
-   * @return code
-  */
-  @ApiModelProperty(example = "SampleError", required = true, value = "The code is either one of the standardized error codes or a custom code, for example specified by a user in the `debug` process.")
-  @NotNull
+	public String getCode() {
+		return code;
+	}
 
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-  public String getCode() {
-    return code;
-  }
+	public Error message(String message) {
+		this.message = message;
+		return this;
+	}
 
-  public void setCode(String code) {
-    this.code = code;
-  }
+	/**
+	 * A message explaining what the client may need to change or what difficulties
+	 * the server is facing.
+	 * 
+	 * @return message
+	 */
+	@ApiModelProperty(example = "Parameter 'sample' is missing.", required = true, value = "A message explaining what the client may need to change or what difficulties the server is facing.")
+	@NotNull
 
-  public Error message(String message) {
-    this.message = message;
-    return this;
-  }
+	public String getMessage() {
+		return message;
+	}
 
-  /**
-   * A message explaining what the client may need to change or what difficulties the server is facing.
-   * @return message
-  */
-  @ApiModelProperty(example = "Parameter 'sample' is missing.", required = true, value = "A message explaining what the client may need to change or what difficulties the server is facing.")
-  @NotNull
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
+	public Error links(List<Link> links) {
+		this.links = links;
+		return this;
+	}
 
-  public String getMessage() {
-    return message;
-  }
+	public Error addLinksItem(Link linksItem) {
+		if (this.links == null) {
+			this.links = new ArrayList<>();
+		}
+		this.links.add(linksItem);
+		return this;
+	}
 
-  public void setMessage(String message) {
-    this.message = message;
-  }
+	/**
+	 * Links related to this log entry / error, e.g. to a resource that provides
+	 * further explanations. For relation types see the lists of [common relation
+	 * types in openEO](#section/API-Principles/Web-Linking).
+	 * 
+	 * @return links
+	 */
+	@ApiModelProperty(example = "[{\"href\":\"https://example.openeo.org/docs/errors/SampleError\",\"rel\":\"about\"}]", value = "Links related to this log entry / error, e.g. to a resource that provides further explanations.  For relation types see the lists of [common relation types in openEO](#section/API-Principles/Web-Linking).")
 
-  public Error links(List<Link> links) {
-    this.links = links;
-    return this;
-  }
+	@Valid
 
-  public Error addLinksItem(Link linksItem) {
-    if (this.links == null) {
-      this.links = new ArrayList<>();
-    }
-    this.links.add(linksItem);
-    return this;
-  }
+	public List<Link> getLinks() {
+		return links;
+	}
 
-  /**
-   * Links related to this log entry / error, e.g. to a resource that provides further explanations.  For relation types see the lists of [common relation types in openEO](#section/API-Principles/Web-Linking).
-   * @return links
-  */
-  @ApiModelProperty(example = "[{\"href\":\"https://example.openeo.org/docs/errors/SampleError\",\"rel\":\"about\"}]", value = "Links related to this log entry / error, e.g. to a resource that provides further explanations.  For relation types see the lists of [common relation types in openEO](#section/API-Principles/Web-Linking).")
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
 
-  @Valid
+	@Override
+	public boolean equals(java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Error error = (Error) o;
+		return Objects.equals(this.id, error.id) && Objects.equals(this.code, error.code)
+				&& Objects.equals(this.message, error.message) && Objects.equals(this.links, error.links);
+	}
 
-  public List<Link> getLinks() {
-    return links;
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, code, message, links);
+	}
 
-  public void setLinks(List<Link> links) {
-    this.links = links;
-  }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class Error {\n");
 
+		sb.append("    id: ").append(toIndentedString(id)).append("\n");
+		sb.append("    code: ").append(toIndentedString(code)).append("\n");
+		sb.append("    message: ").append(toIndentedString(message)).append("\n");
+		sb.append("    links: ").append(toIndentedString(links)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Error error = (Error) o;
-    return Objects.equals(this.id, error.id) &&
-        Objects.equals(this.code, error.code) &&
-        Objects.equals(this.message, error.message) &&
-        Objects.equals(this.links, error.links);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, code, message, links);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Error {\n");
-    
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    code: ").append(toIndentedString(code)).append("\n");
-    sb.append("    message: ").append(toIndentedString(message)).append("\n");
-    sb.append("    links: ").append(toIndentedString(links)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-
--- src/main/java/org/openeo/spring/model/Process.java
+++ src/main/java/org/openeo/spring/model/Process.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -13,7 +14,10 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapKey;
 import javax.persistence.MapKeyColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
@@ -63,11 +67,12 @@ public class Process {
 
 	@JsonProperty("exceptions")
 	@Valid
-	@ElementCollection
-	@CollectionTable(name = "process_exception", joinColumns = {@JoinColumn(name = "process_id", referencedColumnName = "id")})
-	@MapKeyColumn(name = "process_name")
-	@Column(name = "exception_object")
-	private Map<String, Object> exceptions = null;
+	@OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "exception_map", 
+      joinColumns = {@JoinColumn(name = "id", referencedColumnName = "id")},
+      inverseJoinColumns = {@JoinColumn(name = "object_id", referencedColumnName = "id")})
+    @MapKey(name = "id")
+	private Map<String, Error> exceptions = null;
 
 	@JsonProperty("examples")
 	@Valid
@@ -79,15 +84,24 @@ public class Process {
 	@Embedded
 	private List<Link> links = null;
 
+//	@JsonProperty("process_graph")
+//	@Valid
+//	@ElementCollection
+//	@CollectionTable(name = "process_graph", 
+//					 joinColumns = {
+//							 @JoinColumn(name = "process_id", referencedColumnName = "id")})
+//	@MapKeyColumn(name = "process_name")
+//	@Column(name = "process_object")
+//	private Map<String, Object> processGraph = null;
+	
 	@JsonProperty("process_graph")
 	@Valid
-	@ElementCollection
-	@CollectionTable(name = "process_graph", 
-					 joinColumns = {
-							 @JoinColumn(name = "process_id", referencedColumnName = "id")})
-	@MapKeyColumn(name = "process_name")
-	@Column(name = "process_object")
-	private Map<String, Object> processGraph = null;
+	@OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "process_graph_map", 
+      joinColumns = {@JoinColumn(name = "id", referencedColumnName = "id")},
+      inverseJoinColumns = {@JoinColumn(name = "object_id", referencedColumnName = "id")})
+    @MapKey(name = "id")
+	private Map<String, Process> processGraph = null;
 
 	public Process id(String id) {
 		this.id = id;
@@ -294,12 +308,12 @@ public class Process {
 		this.experimental = experimental;
 	}
 
-	public Process exceptions(Map<String, Object> exceptions) {
+	public Process exceptions(Map<String, Error> exceptions) {
 		this.exceptions = exceptions;
 		return this;
 	}
 
-	public Process putExceptionsItem(String key, Object exceptionsItem) {
+	public Process putExceptionsItem(String key, Error exceptionsItem) {
 		if (this.exceptions == null) {
 			this.exceptions = new HashMap<>();
 		}
@@ -319,11 +333,11 @@ public class Process {
 	 */
 	@ApiModelProperty(value = "Declares any exceptions (errors) that might occur during execution of this process. This list is just for informative purposes and may be incomplete. This list MUST only contain exceptions that stop the execution of a process and MUST NOT contain warnings, notices or debugging messages.  The keys define the error code and MUST match the following pattern: `^\\w+$`  This schema follows the schema of the general openEO error list (see errors.json).")
 
-	public Map<String, Object> getExceptions() {
+	public Map<String, Error> getExceptions() {
 		return exceptions;
 	}
 
-	public void setExceptions(Map<String, Object> exceptions) {
+	public void setExceptions(Map<String, Error> exceptions) {
 		this.exceptions = exceptions;
 	}
 
@@ -396,12 +410,12 @@ public class Process {
 		this.links = links;
 	}
 
-	public Process processGraph(Map<String, Object> processGraph) {
+	public Process processGraph(Map<String, Process> processGraph) {
 		this.processGraph = processGraph;
 		return this;
 	}
 
-	public Process putProcessGraphItem(String key, Object processGraphItem) {
+	public Process putProcessGraphItem(String key, Process processGraphItem) {
 		if (this.processGraph == null) {
 			this.processGraph = new HashMap<>();
 		}
@@ -418,11 +432,11 @@ public class Process {
 	 */
 	@ApiModelProperty(example = "{\"dc\":{\"process_id\":\"load_collection\",\"arguments\":{\"id\":\"Sentinel-2\",\"spatial_extent\":{\"west\":16.1,\"east\":16.6,\"north\":48.6,\"south\":47.2},\"temporal_extent\":[\"2018-01-01\",\"2018-02-01\"]}},\"bands\":{\"process_id\":\"filter_bands\",\"description\":\"Filter and order the bands. The order is important for the following reduce operation.\",\"arguments\":{\"data\":{\"from_node\":\"dc\"},\"bands\":[\"B08\",\"B04\",\"B02\"]}},\"evi\":{\"process_id\":\"reduce\",\"description\":\"Compute the EVI. Formula: 2.5 * (NIR - RED) / (1 + NIR + 6*RED + -7.5*BLUE)\",\"arguments\":{\"data\":{\"from_node\":\"bands\"},\"dimension\":\"bands\",\"reducer\":{\"process_graph\":{\"nir\":{\"process_id\":\"array_element\",\"arguments\":{\"data\":{\"from_parameter\":\"data\"},\"index\":0}},\"red\":{\"process_id\":\"array_element\",\"arguments\":{\"data\":{\"from_parameter\":\"data\"},\"index\":1}},\"blue\":{\"process_id\":\"array_element\",\"arguments\":{\"data\":{\"from_parameter\":\"data\"},\"index\":2}},\"sub\":{\"process_id\":\"subtract\",\"arguments\":{\"data\":[{\"from_node\":\"nir\"},{\"from_node\":\"red\"}]}},\"p1\":{\"process_id\":\"product\",\"arguments\":{\"data\":[6,{\"from_node\":\"red\"}]}},\"p2\":{\"process_id\":\"product\",\"arguments\":{\"data\":[-7.5,{\"from_node\":\"blue\"}]}},\"sum\":{\"process_id\":\"sum\",\"arguments\":{\"data\":[1,{\"from_node\":\"nir\"},{\"from_node\":\"p1\"},{\"from_node\":\"p2\"}]}},\"div\":{\"process_id\":\"divide\",\"arguments\":{\"data\":[{\"from_node\":\"sub\"},{\"from_node\":\"sum\"}]}},\"p3\":{\"process_id\":\"product\",\"arguments\":{\"data\":[2.5,{\"from_node\":\"div\"}]},\"result\":true}}}}},\"mintime\":{\"process_id\":\"reduce\",\"description\":\"Compute a minimum time composite by reducing the temporal dimension\",\"arguments\":{\"data\":{\"from_node\":\"evi\"},\"dimension\":\"temporal\",\"reducer\":{\"process_graph\":{\"min\":{\"process_id\":\"min\",\"arguments\":{\"data\":{\"from_parameter\":\"data\"}},\"result\":true}}}}},\"save\":{\"process_id\":\"save_result\",\"arguments\":{\"data\":{\"from_node\":\"mintime\"},\"format\":\"GTiff\"},\"result\":true}}", value = "A process graph defines a graph-like structure as a connected set of executable processes. Each key is a unique identifier (node ID) that is used to refer to the process in the graph.")
 
-	public Map<String, Object> getProcessGraph() {
+	public Map<String, Process> getProcessGraph() {
 		return processGraph;
 	}
 
-	public void setProcessGraph(Map<String, Object> processGraph) {
+	public void setProcessGraph(Map<String, Process> processGraph) {
 		this.processGraph = processGraph;
 	}
 
