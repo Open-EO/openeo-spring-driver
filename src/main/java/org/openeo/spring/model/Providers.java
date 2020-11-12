fc69053c35a81429b454969ee96bb8861850716b
Budhwar Prateek <prateek.budhwar@eurac.edu>
2020-11-12 19:51:49 +0000

    added bands, summaries and providers model file

---
+++ src/main/java/org/openeo/spring/model/Providers.java
@@ -0,0 +1,162 @@
+package org.openeo.spring.model;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * A link to another resource on the web. Bases on [RFC 5899](https://tools.ietf.org/html/rfc5988).
+ */
+@Embeddable
+@ApiModel(description = "A link to another resource on the web. Bases on [RFC 5899](https://tools.ietf.org/html/rfc5988).")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-07-02T08:45:00.334+02:00[Europe/Rome]")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Providers   {
+  @JsonProperty("name")
+  private String name;
+
+  @JsonProperty("url")
+  private URI url;
+
+  @JsonProperty("roles")
+  private List<String> roles;
+
+  @JsonProperty("title")
+  private String title;
+
+  public Providers name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Relationship between the current document and the linked document. SHOULD be a [registered link relation type](https://www.iana.org/assignments/link-relations/link-relations.xml) whenever feasible.
+   * @return rel
+  */
+  @ApiModelProperty(example = "EURAC", required = true, value = "Relationship between the current document and the linked document. SHOULD be a [registered link relation type](https://www.iana.org/assignments/link-relations/link-relations.xml) whenever feasible.")
+  @NotNull
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Providers url(URI url) {
+    this.url = url;
+    return this;
+  }
+
+  /**
+   * The value MUST be a valid URL.
+   * @return href
+  */
+  @ApiModelProperty(example = "https://example.openeo.org", required = true, value = "The value MUST be a valid URL.")
+  @NotNull
+
+  @Valid
+
+  public URI getUrl() {
+    return url;
+  }
+
+  public void setUrl(URI url) {
+    this.url = url;
+  }
+
+  public Providers roles(List<String> roles) {
+    this.roles = roles;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "[producer, host]", value = "The value MUST be a list of strings that gives the providers status.")
+
+
+  public List<String> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<String> roles) {
+    this.roles = roles;
+  }
+
+  public Providers title(String title) {
+    this.title = title;
+    return this;
+  }
+
+  /**
+   * Used as a human-readable label for a link.
+   * @return title
+  */
+  @ApiModelProperty(example = "openEO", value = "Used as a human-readable label for a link.")
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Providers providers = (Providers) o;
+    return Objects.equals(this.name, providers.name) &&
+        Objects.equals(this.url, providers.url) &&
+        Objects.equals(this.roles, providers.roles) &&
+        Objects.equals(this.title, providers.title);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, url, roles, title);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Providers {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    url: ").append(toIndentedString(url)).append("\n");
+    sb.append("    roles: ").append(toIndentedString(roles)).append("\n");
+    sb.append("    title: ").append(toIndentedString(title)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
