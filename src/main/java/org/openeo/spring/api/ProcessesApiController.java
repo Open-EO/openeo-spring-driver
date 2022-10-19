9b764a9b7b9f932856ec594e892572323b12d75e
pcampalani <piero.campalani@eurac.edu>
2022-10-19 13:03:23 +0200

    Do not let a single missing/broken processes list file joepardise the whole harvesting.

--- src/main/java/org/openeo/spring/api/ProcessesApiController.java
+++ src/main/java/org/openeo/spring/api/ProcessesApiController.java
@@ -1,29 +1,25 @@
 package org.openeo.spring.api;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-import org.openeo.spring.model.Processes;
-import org.openeo.spring.model.Process;
-import org.openeo.spring.model.ProcessParameter;
-import org.openeo.spring.model.ProcessReturnValue;
-import org.openeo.spring.model.DataTypeSchema;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.ProcessIdUtil;
 import org.openeo.spring.model.EngineTypes;
 import org.openeo.spring.model.Link;
-import org.openeo.spring.model.ParameterSchema;
-
-import java.util.Map;
-
+import org.openeo.spring.model.Process;
+import org.openeo.spring.model.Processes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
@@ -37,60 +33,64 @@ import org.springframework.web.context.request.NativeWebRequest;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Parameter;
+
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-07-02T08:45:00.334+02:00[Europe/Rome]")
 @RestController
 @RequestMapping("${openapi.openEO.base-path:}")
 public class ProcessesApiController implements ProcessesApi {
-    private final NativeWebRequest request;    
+
+    private static final Logger log = LogManager.getLogger(ProcessIdUtil.class);
+
+    private final NativeWebRequest request;
     private Map<String, Process> processes = null;
 	private Map<String, Link> links = null;
 	private ObjectMapper mapper = null;
-//	("classpath:processes.json")
+
 	@Value("${org.openeo.wcps.processes.list}")
 	Resource processesFileWCPS;
+
 	@Value("${org.openeo.odc.processes.list}")
 	Resource processesFileODC;
-	
-//	
+
 	@Autowired
 	ResourceLoader resourceLoader;
-	
+
     @org.springframework.beans.factory.annotation.Autowired
     public ProcessesApiController(NativeWebRequest request) {
         this.request = request;
-        
+
 //        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 //        new ClassPathResource("processes.json").getInputStream();
 //        Resource processResource = resourceLoader.getResource("classpath:processes.json");
 //        Resource linksResource = resourceLoader.getResource("classpath:links.json");
-        
+
 //    	InputStream stream = processResource.getInputStream();
 //    	InputStream linkstream = linksResource.getInputStream();
-    	
+
 //    	InputStream stream = classLoader.getResourceAsStream("processes.json");
 //    	InputStream linkstream = classLoader.getResourceAsStream("links.json");
-    	
+
 //    	this.mapper = new ObjectMapper();
 //    	this.processes = new HashMap<String, Process>();
 //    	this.links = new HashMap<String, Link>();
-//    	
+//
 //    	try {
 //			Process[] processArray = this.mapper.readValue(stream, Process[].class);
 //			Link[] linksArray = this.mapper.readValue(linkstream, Link[].class);
-//			
+//
 //			for(int p = 0; p < linksArray.length; p++) {
-//				this.links.put(linksArray[p].getRel(), linksArray[p]);				
+//				this.links.put(linksArray[p].getRel(), linksArray[p]);
 ////				log.debug("Found and stored process: " + linksArray[p].getRel());
 //			}
-//			
+//
 //			for(int p = 0; p < processArray.length; p++) {
-//				this.processes.put(processArray[p].getId(), processArray[p]);				
+//				this.processes.put(processArray[p].getId(), processArray[p]);
 ////				log.debug("Found and stored process: " + processArray[p].getId());
-//			}			
+//			}
 //		} catch (JsonParseException e) {
 ////			log.error("Error parsing json: " + e.getMessage());
 //		} catch (JsonMappingException e) {
@@ -104,7 +104,7 @@ public class ProcessesApiController implements ProcessesApi {
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
     }
-    
+
     /**
      * GET /processes : Supported predefined processes
      * The request asks the back-end for available predefined processes and returns detailed process descriptions, including parameters and return values.
@@ -115,50 +115,50 @@ public class ProcessesApiController implements ProcessesApi {
      *         or The request can&#39;t be fulfilled due to an error at the back-end. The error is never the client’s fault and therefore it is reasonable for the client to retry the exact same request that triggered this response.  The response body SHOULD contain a JSON error object. MUST be any HTTP status code specified in [RFC 7231](https://tools.ietf.org/html/rfc7231#section-6.6).  See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json) (status code 500)
      */
     @Override
-	@Operation(summary = "Supported predefined processes", operationId = "listProcesses", description = "The request asks the back-end for available predefined processes and returns detailed process descriptions, including parameters and return values.", tags={ "Process Discovery", })
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "200", description = "Formal specification describing the supported predefined processes."),
-        @ApiResponse(responseCode = "400", description = "The request can't be fulfilled due to an error on client-side, i.e. the request is invalid. The client should not repeat the request without modifications.  The response body SHOULD contain a JSON error object. MUST be any HTTP status code specified in [RFC 7231](https://tools.ietf.org/html/rfc7231#section-6.6). This request MUST respond with HTTP status codes 401 if authorization is required or 403 if the authorization failed or access is forbidden in general to the authenticated user. HTTP status code 404 should be used if the value of a path parameter is invalid.  See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json)"),
-        @ApiResponse(responseCode = "500", description = "The request can't be fulfilled due to an error at the back-end. The error is never the client’s fault and therefore it is reasonable for the client to retry the exact same request that triggered this response.  The response body SHOULD contain a JSON error object. MUST be any HTTP status code specified in [RFC 7231](https://tools.ietf.org/html/rfc7231#section-6.6).  See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json)") })
+    @Operation(summary = "Supported predefined processes", operationId = "listProcesses", description = "The request asks the back-end for available predefined processes and returns detailed process descriptions, including parameters and return values.", tags={ "Process Discovery", })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Formal specification describing the supported predefined processes."),
+            @ApiResponse(responseCode = "400", description = "The request can't be fulfilled due to an error on client-side, i.e. the request is invalid. The client should not repeat the request without modifications.  The response body SHOULD contain a JSON error object. MUST be any HTTP status code specified in [RFC 7231](https://tools.ietf.org/html/rfc7231#section-6.6). This request MUST respond with HTTP status codes 401 if authorization is required or 403 if the authorization failed or access is forbidden in general to the authenticated user. HTTP status code 404 should be used if the value of a path parameter is invalid.  See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json)"),
+            @ApiResponse(responseCode = "500", description = "The request can't be fulfilled due to an error at the back-end. The error is never the client’s fault and therefore it is reasonable for the client to retry the exact same request that triggered this response.  The response body SHOULD contain a JSON error object. MUST be any HTTP status code specified in [RFC 7231](https://tools.ietf.org/html/rfc7231#section-6.6).  See also: * [Error Handling](#section/API-Principles/Error-Handling) in the API in general. * [Common Error Codes](errors.json)") })
     @GetMapping(value = "/processes", produces = { "application/json" })
-    
+
     public ResponseEntity<Processes> listProcesses(@Min(1)@Parameter(description = "This parameter enables pagination for the endpoint and specifies the maximum number of elements that arrays in the top-level object (e.g. jobs or log entries) are allowed to contain. The only exception is the `links` array, which MUST NOT be paginated as otherwise the pagination links may be missing ins responses. If the parameter is not provided or empty, all elements are returned.  Pagination is OPTIONAL and back-ends and clients may not support it. Therefore it MUST be implemented in a way that clients not supporting pagination get all resources regardless. Back-ends not supporting  pagination will return all resources.  If the response is paginated, the links array MUST be used to propagate the  links for pagination with pre-defined `rel` types. See the links array schema for supported `rel` types.  *Note:* Implementations can use all kind of pagination techniques, depending on what is supported best by their infrastructure. So it doesn't care whether it is page-based, offset-based or uses tokens for pagination. The clients will use whatever is specified in the links with the corresponding `rel` types.") @Valid @RequestParam(value = "limit", required = false) Integer limit) {
-    	ObjectMapper mapper = new ObjectMapper();
-    	Processes processesList = new Processes();
-    	HashMap<String, Process> processMap = new HashMap<String, Process>();
-		try {
-			Processes processesListWCPS = mapper.readValue(processesFileWCPS.getInputStream(), Processes.class);
-			Processes processesListODC = mapper.readValue(processesFileODC.getInputStream(), Processes.class);
-			for(Process processElement: processesListWCPS.getProcesses()) {
-				processElement.addEngine(EngineTypes.WCPS);
-				processMap.put(processElement.getId(),processElement);
-				//processesList.addProcessesItem(processElement);
-			}
-			for(Process processElement: processesListODC.getProcesses()) {
-				Process currentProcess = processMap.get(processElement.getId());
-				if (currentProcess == null) {
-					processElement.addEngine(EngineTypes.ODC_DASK);
-					processMap.put(processElement.getId(),processElement);					
-				}
-				else {
-					processMap.get(processElement.getId()).addEngine(EngineTypes.ODC_DASK);
-				}
-				//processesList.addProcessesItem(processElement);
-			}
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-//    	
+
+        ObjectMapper mapper = new ObjectMapper();
+        Processes processesList = new Processes();
+        Map<String, Process> processMap = new HashMap<String, Process>();
+
+        final Map<Resource, EngineTypes> res2eng = new LinkedHashMap<>();
+        res2eng.put(processesFileODC, EngineTypes.ODC_DASK);
+        res2eng.put(processesFileWCPS, EngineTypes.WCPS);
+
+        for (Resource processesFile : res2eng.keySet()) {
+            try (InputStream is = processesFile.getInputStream();){
+                Processes processesListEng = mapper.readValue(is, Processes.class);
+                EngineTypes engine = res2eng.get(processesFile);
+
+                for(Process processElement: processesListEng.getProcesses()) {
+                    final String pID = processElement.getId();
+                    Process existingProcess = processMap.get(pID);
+                    if (null != existingProcess) {
+                        existingProcess.addEngine(engine);
+                        log.debug("Appended {} engine to process '{}'", engine, pID);
+                    } else {
+                        processElement.addEngine(engine);
+                        processMap.put(pID,processElement);
+                        log.debug("Stored new process for engine {}: '{}'", engine, pID);
+                    }
+                }
+            } catch (JsonParseException | JsonMappingException e) {
+                log.warn("Invalid processes file {}.", processesFile, e); // be lenient?
+            } catch (IOException e) {
+                log.warn("Cannot access processes file {}.", processesFile, e); // be lenient?
+            }
+        }
+//
 ////    	for(String key : this.processes.keySet()){
 ////			Process process = this.processes.get(key);
-////			Process processDef = new Process();	    	
+////			Process processDef = new Process();
 ////	    	processDef.setId(process.getId());
 ////			processDef.setSummary(process.getSummary());
 ////			processDef.setDescription(process.getDescription());
@@ -167,9 +167,9 @@ public class ProcessesApiController implements ProcessesApi {
 ////			processDef.setReturns(process.getReturns());
 ////			processesList.addProcessesItem(processDef);
 ////		}
-//    	
-//    	
-//    	
+//
+//
+//
 //    	Process process1 = new Process ();
 //    	process1.setId("load_collection");
 //    	process1.setSummary("Load a collection");
@@ -194,15 +194,15 @@ public class ProcessesApiController implements ProcessesApi {
 //    	process1.addParametersItem(parametersItem1ID);
 //    	process1.addParametersItem(parametersItem1SpatExt);
 //    	process1.addParametersItem(parametersItem1TempExt);
-//    	
+//
 //    	ProcessReturnValue returns1 = new ProcessReturnValue();
 //    	DataTypeSchema schemaReturn1 = new DataTypeSchema();
 //    	returns1.setDescription("A data cube for further processing. The dimensions and dimension properties (name, type, labels, reference system and resolution) correspond to the collection's metadata, but the dimension labels are restricted as specified in the parameters.");
-//    	
+//
 ////    	returns1.setSchema(schemaReturn1);
 //    	process1.setReturns(returns1);
-//    	
-//    	
+//
+//
 //    	Process process2 = new Process ();
 //    	process2.setId("run_udf");
 //    	process2.setSummary("Run an UDF");
@@ -219,20 +219,20 @@ public class ProcessesApiController implements ProcessesApi {
 //    	parametersItem2Data.setName("data");
 //    	parametersItem2UDF.setName("udf");
 //    	parametersItem2Runtime.setName("runtime");
-//    	parametersItem2Data.setDescription("The data to be passed to the UDF as array or raster data cube.");    	
+//    	parametersItem2Data.setDescription("The data to be passed to the UDF as array or raster data cube.");
 //    	parametersItem2UDF.setDescription("Either source code, an absolute URL or a path to an UDF script.");
 //    	parametersItem2Runtime.setDescription("An UDF runtime identifier available at the back-end.");
 //    	process2.addParametersItem(parametersItem2Data);
 //    	process2.addParametersItem(parametersItem2UDF);
 //    	process2.addParametersItem(parametersItem2Runtime);
-//    	
+//
 //    	ProcessReturnValue returns2 = new ProcessReturnValue();
 //    	DataTypeSchema schemaReturn2 = new DataTypeSchema();
-//    	
+//
 //    	returns2.setDescription("The data processed by the UDF.\\n\\n* Returns a raster data cube, if a raster data cube is passed for `data`. Details on the dimensions and dimension properties (name, type, labels, reference system and resolution) depend on the UDF.\\n* If an array is passed for `data`, the returned value can be of any data type, but is exactly what the UDF returns.");
 //    	process2.setReturns(returns2);
-//    	
-//    	
+//
+//
 //    	Process process3 = new Process ();
 //    	process3.setId("save_result");
 //    	process3.setSummary("Save processed data to storage");
@@ -250,25 +250,25 @@ public class ProcessesApiController implements ProcessesApi {
 //    	parametersItem3Format.setDescription("The file format to save to. It must be one of the values that the server reports as supported output file formats, which usually correspond to the short GDAL/OGR codes. If the format is not suitable for storing the underlying data structure, a `FormatUnsuitable` exception will be thrown. This parameter is *case insensitive*.");
 //    	process3.addParametersItem(parametersItem3Data);
 //    	process3.addParametersItem(parametersItem3Format);
-//    	
+//
 //    	ProcessReturnValue returns3 = new ProcessReturnValue();
 //    	DataTypeSchema schemaReturn3 = new DataTypeSchema();
-//    	
+//
 //    	returns2.setDescription("false` if saving failed, `true` otherwise.");
 //    	process2.setReturns(returns3);
-//    	
-//    	
+//
+//
 //    	processesList.addProcessesItem(process1);
 //    	processesList.addProcessesItem(process2);
 //    	processesList.addProcessesItem(process3);
-    	
-    	
+
+
 //    	getRequest().ifPresent(request -> {
 //            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
 //                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
 //                	String exampleString = "{ \"processes\" : [ \"{}\", \"{}\" ], \"links\" : [ { \"rel\" : \"related\", \"href\" : \"https://example.openeo.org\", \"type\" : \"text/html\", \"title\" : \"openEO\" }, { \"rel\" : \"related\", \"href\" : \"https://example.openeo.org\", \"type\" : \"text/html\", \"title\" : \"openEO\" } ] }";
 //                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
-//                    
+//
 //                }
 //            }
 //        });
@@ -276,7 +276,5 @@ public class ProcessesApiController implements ProcessesApi {
 			processesList.addProcessesItem(proc);
 		}
         return new ResponseEntity<Processes>(processesList, HttpStatus.OK);
-
     }
-    
 }
