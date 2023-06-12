e0859da6886fd1cf9506dde602fe2dd321d56395
pcampalani <piero.campalani@eurac.edu>
2023-06-12 19:34:51 +0200

    Update launch script.

--- launch_openeo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export JAVA_HOME="..."
-java -Xmx16G \
-     -jar \
-     -Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts \
-     -Djavax.net.ssl.trustStorePassword=changeit \
-     ./target/openeo-spring-driver-1.0.0-draft.jar
