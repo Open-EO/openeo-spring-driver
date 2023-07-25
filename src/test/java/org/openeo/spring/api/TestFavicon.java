7d0c8027eb3049b86cc640ad3220bd304aac6d40
pcampalani <piero.campalani@eurac.edu>
2023-07-25 18:31:58 +0200

    Add test for favicon request.

---
+++ src/test/java/org/openeo/spring/api/TestFavicon.java
@@ -0,0 +1,29 @@
+package org.openeo.spring.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(FaviconController.class)
+public class TestFavicon {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getFavicon_shouldReturn200() throws Exception {
+        mvc.perform(get("/favicon.ico")
+        ).andExpectAll(
+                status().is(200),
+                content().contentType(MediaType.valueOf("image/x-icon")));
+    }
+}
